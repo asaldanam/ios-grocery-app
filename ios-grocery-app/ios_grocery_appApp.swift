c69dbad705f6ebaa58388b220dad15fa12ab0bef
asaldanam <asaldanam@gmail.com>
2024-03-02 12:16:56 +0100

    Initial Commit

---
+++ ios-grocery-app/ios_grocery_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_grocery_appApp.swift
+//  ios-grocery-app
+//
+//  Created by Abel Saldaña Martínez on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_grocery_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
